78d2b629e3454ad8c69d5a5916a47197f7024afb
Robert Wong <robertjwong246@gmail.com>
2017-01-21 18:19:00 -0500

    Added a random wind component, enlarged the whale-wave and added colliders to both components

--- KillerTide/Assets/Scripts/GameController.cs
+++ KillerTide/Assets/Scripts/GameController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+	private float windPowerCurrent = 0f;
+	private float windDelay = 5f;
+	private float windTimer = 0f;
+	private float windPowerPrevious = 0f;
+	public bool windSwitch = false;
+
+	[SerializeField]
+	private Rigidbody2D whaleRB;
+	[SerializeField]
+	private Rigidbody2D surferRB;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (windTimer < windDelay) {
+			windTimer = windTimer + Time.deltaTime;
+		}
+		if (windTimer > windDelay && windSwitch == true){
+			windPowerCurrent = Random.Range(-1.5f, 1.5f);
+			if (windPowerCurrent > 0f) {
+				whaleRB.AddForce (new Vector2 (windPowerCurrent, 0), ForceMode2D.Impulse);
+				surferRB.AddForce (new Vector2 (windPowerCurrent, 0), ForceMode2D.Impulse);
+			} else if (windPowerCurrent < 0f)
+			{
+				whaleRB.AddForce(new Vector2 (-windPowerCurrent ,0), ForceMode2D.Impulse);
+				surferRB.AddForce(new Vector2 (-windPowerCurrent ,0), ForceMode2D.Impulse);
+			}
+			windTimer = 0;
+			print (windPowerCurrent);
+		}
 	}
 }
