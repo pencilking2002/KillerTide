b28ff96ca4927f35b42620c85671756e1c8640d5
Roman <pencilking2002@gmail.com>
2017-01-22 13:54:18 -0500

    Added Game Menu

--- KillerTide/Assets/Scripts/GameController.cs
+++ KillerTide/Assets/Scripts/GameController.cs
@@ -3,23 +3,95 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+
+	public static GameController Instance;
+
+	[Header("Game Menu controls")]
+	public GameObject hyperCube;
+	public bool startFromGameMenu = true;
+	public Transform GameStartPoint;
+
 	private float windPowerCurrent = 0f;
 	private float windDelay = 5f;
 	private float windTimer = 0f;
 	private float windPowerPrevious = 0f;
 	public bool windSwitch = false;
 
+	public Camera cam;
+	public GameObject canvas;
+
 	[SerializeField]
 	private Rigidbody2D whaleRB;
 	[SerializeField]
 	private Rigidbody2D surferRB;
 
-	// Use this for initialization
-	void Start () {
+	// This will be assigned either the camera or the hyper cube group
+	private GameObject cameraToMove;
+
+	public enum GameState{
+		Menu,
+		Playing,
+		SurferWin,
+		WhaleWin
 	}
-	
+	public GameState state = GameState.Menu;
+
+	void Awake()
+	{
+		if (Instance == null)
+			Instance = this;
+		else 
+			Destroy(gameObject);
+
+		if (startFromGameMenu)
+			SetMenuState();
+		else
+			SetPlayingState();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (IsMenuState())
+		{
+			if (hyperCube.activeInHierarchy)
+			{
+				hyperCube.transform.position = 
+					new Vector3(hyperCube.transform.position.x, canvas.transform.position.y, hyperCube.transform.position.z);
+
+				cameraToMove = hyperCube;
+			}
+			else
+			{
+				print("move cam");
+				cam.transform.position = 
+					new Vector3(cam.transform.position.x, canvas.transform.position.y, cam.transform.position.z);
+
+				cameraToMove = cam.gameObject;
+			
+			}
+		}
+
+
+	}
+
+
+
+	void OnGUI()
+	{
+		GUI.Button(new Rect(10,10,100,40), WhaleController.Instance.GetWhaleState().ToString());
+		GUI.Button(new Rect(10,50,100,40), GetGameState().ToString());
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			print("start");
+			CameraStartGame();
+		}
+
 		if (windTimer < windDelay) {
 			windTimer = windTimer + Time.deltaTime;
 		}
@@ -36,5 +108,52 @@ public class GameController : MonoBehaviour {
 			windTimer = 0;
 			print (windPowerCurrent);
 		}
+
+
 	}
+
+	public void CameraStartGame()
+	{
+		var targetPos = new Vector3(cameraToMove.transform.position.x, GameStartPoint.position.y, cameraToMove.transform.position.z);
+
+		if (!hyperCube.activeInHierarchy)
+			targetPos.y = 0;
+
+		print("start game");
+		LeanTween.move(cameraToMove,targetPos, 2.0f).setEaseOutQuint().setDelay(0.2f)
+			.setOnComplete(() => {
+			SetPlayingState();
+		});
+
+	}
+
+
+	// GAme state
+
+	public void SetMenuState() { state = GameState.Menu; }
+	public void SetPlayingState() 
+	{ 
+		state = GameState.Playing;
+		StartGame(); 
+	}
+	public void SetWhaleWin() { state = GameState.WhaleWin; }
+	public void SetSurferWin() { state = GameState.SurferWin; }
+
+
+	public bool IsMenuState() { return state == GameState.Menu; }
+	public bool IsPlayingState() 
+	{ 
+		
+		return state == GameState.Playing; 
+	}
+
+	public bool IsWhaleWinState() { return state == GameState.WhaleWin; }
+	public bool IsSurferWinState() { return state == GameState.SurferWin; }
+	public GameState GetGameState() { return state; }
+
+	public void StartGame()
+	{
+		WhaleController.Instance.StartWhaleProcess();
+	}
+
 }
