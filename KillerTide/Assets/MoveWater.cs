98d92ccecb94464a90c956e95542fa0f72bd3f0e
Roman <pencilking2002@gmail.com>
2017-01-21 19:35:45 -0500

    Added graphics

---
+++ KillerTide/Assets/MoveWater.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWater : MonoBehaviour {
+
+	public float moveSpeed = 0.5f;
+	public bool reset = false;
+
+	public GameObject waterDetailPrefab;
+	private Vector3 origPos;
+	private BoxCollider2D bCollider;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		origPos = transform.position;
+		bCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+		Vector3 screenWorldRightEdge = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,0,-10));
+		Vector3 screenWorldLeftEdge = Camera.main.ScreenToWorldPoint(new Vector3(0,0,-10));
+
+
+		if (transform.position.x > screenWorldLeftEdge.x - bCollider.bounds.size.x)
+		{
+			transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0,0);
+		}
+		else
+		{
+
+			Vector3 targetPos = origPos;
+			targetPos.x = screenWorldRightEdge.x;
+	
+			transform.position = targetPos;
+		}
+
+				
+	}
+//
+//	void OnBecameInvisible()
+//	{
+//		print ("invisible");
+//		visible = false;
+//
+//	}
+
+}
---
+++ KillerTide/Assets/Oscilation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilation : MonoBehaviour {
+
+	public float tweenTime = 0.2f;
+	public Vector3 offset = new Vector3(0.2f,0.2f,0);
+	// Use this for initialization
+	void Start () 
+	{
+		var targetPos = transform.position + offset;
+		LeanTween.move(gameObject, targetPos, tweenTime).setLoopPingPong(-1);
+	}
+
+}
